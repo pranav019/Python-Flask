1dade1c96e13a08d3d6c03bd9799e95cae6f35b1
RanaPranav22 <pranavrana019@gmail.com>
2024-12-18 11:20:16 +0530

    Basics

---
+++ 01_variables/code.py
@@ -0,0 +1,19 @@
+x= 15
+price = 9.99
+discount = 0.2
+result = price * (1-discount)
+print(result)
+# Strings
+name = "Rofl"
+# Re-assigning allowed
+name = "Pranav"
+print(name)
+print(name*2)
+# changing variables
+a=25
+b=a
+print(a)
+print(b)
+b=17
+print(a)
+print(b)
